7cd604238202433c8e3d970aabd5da8e9e24797e
Keez Eden <keezeden@gmail.com>
2021-07-02 09:24:24 +1200

    Edited career/index.md

---
+++ career/index.md
@@ -0,0 +1,5 @@
+# Career Development
+
+## Resources
+
+- https://css-tricks.com/the-importance-of-career-laddering/
--- knowledge.sh
+++ knowledge.sh
@@ -1,4 +1,4 @@
-read -p fullPath "Path: "
+read -p "Path: " fullPath
 nano $fullPath
 
 { git add --all . ; git commit -m "Edited $fullPath" ; git push; } &
