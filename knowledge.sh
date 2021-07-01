6b01f4f9a66cbea822ebe39ac44e9c43718b92e8
Keez Eden <keezeden@gmail.com>
2021-07-01 15:41:15 +1200

    Nano workflow

--- knowledge.sh
+++ knowledge.sh
@@ -6,13 +6,12 @@ echo "Folder?"
 read folderName
 echo "File?"
 read fileName
-echo "Content?"
-read content
-echo "Writing content to $folderName/$fileName"
-
 
 mkdir $folderName
 cd $folderName
-echo "$content" >> $fileName
+nano $fileName
+
+
+
 
 { git add --all . ; git commit -m "Edited $folderName/$fileName" ; git push; } &
